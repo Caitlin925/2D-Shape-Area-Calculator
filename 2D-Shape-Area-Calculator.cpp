a26c2d3e7bb058d41edbea73227f61c3aaca5e0c
Caitlin Ochuwa <66971152+Caitlin925@users.noreply.github.com>
2025-09-25 18:39:42 -0500

    Update 2D-Shape-Area-Calculator.cpp added comments

--- 2D-Shape-Area-Calculator.cpp
+++ 2D-Shape-Area-Calculator.cpp
@@ -6,48 +6,54 @@
 #include <iomanip>
 using namespace std; 
 
-void areaOfTraingle()
+void areaOfTraingle() //Function to calculate area of a triangle
 {
+    //User enters length and height to be calculated 
     double length, height, area;
     cout << "Enter the length: ";
     cin >> length;
     cout << "Enter the height: ";
     cin >> height;
-    area = (length*height)/2;
-    cout << "The area of the triangle is " << area << endl;
+    area = (length*height)/2; //Area of a triangle is length multiplied by the height divided by 2
+    cout << "The area of the triangle is " << area << endl; //Prints area after using formula 
 }
 
-void areaOfSquare()
+void areaOfSquare() //Function to calculate area of a square
 {
+    //User enters length to be calculated 
     double length, area;
     cout << "Enter the length: ";
     cin >> length;
-    area = pow(length, 2);
-    cout << "The area of the square is " << area << endl;
+    area = pow(length, 2); //Area of a square is the length squared 
+    cout << "The area of the square is " << area << endl; //Prints area after using formula 
 }
 
-void areaOfCircle()
+void areaOfCircle() //Function to calculate area of a circle
 {
-    const double PI = 3.14;
+    //User enters radius to be calculated 
+    const double PI = 3.14;  
     double radius, area;
     cout << "Enter the radius: ";
     cin >> radius;
-    area = PI*(pow(radius, 2.0));
-    cout << "The area of the circle is " << area << endl;
+    area = PI*(pow(radius, 2.0)); //Area of a circle is pi multiplied by the radius squared 
+    cout << "The area of the circle is " << area << endl; //Prints area after using formula 
 }
 
-void areaOfRectangle()
+void areaOfRectangle() //Function to calculate area of a rectangle 
 {
+    //User enters length and width to be calculated 
     double length, width, area;
     cout << "Enter the length: ";
     cin >> length;
     cout << "Enter the width: ";
     cin >> width;
-    area = length*width;
-    cout << "The area of the rectangle is " << area << endl;
+    area = length*width; //Area of a rectangle is the length multiplied by the width
+    cout << "The area of the rectangle is " << area << endl; //Prints area after using formula 
 }
 
-void areaOfTrapezoid(){
+void areaOfTrapezoid() //Function to calculate area of a trapezoid 
+{
+    //User enters top and bottom lengths and height to be calculated 
     double length1, length2, height, area;
     cout << "Enter the length of the top: ";
     cin >> length1;
@@ -55,19 +61,22 @@ void areaOfTrapezoid(){
     cin >> length2;
     cout << "Enter the height: ";
     cin >> height;
-    area = ((length1 + length2)/2)*height;
-    cout << "The area of the trapezoid is " << area << endl;
+    area = ((length1 + length2)/2)*height; //Area of a trapezoid is the average of the top and bottom lengths multiplied by the height
+    cout << "The area of the trapezoid is " << area << endl; //Prints area after using formula 
 }
 
 int main() 
 {
-    string shape;
-    int decimalPlaces;
+    string shape; //Stores the shape the user enters
+    int decimalPlaces; //Stores the number of decimal places the user enters
+    
     cout << "What area would you like to calculate?(Triangle, Square, Circle, Rectangle, Trapezoid): ";
     getline(cin, shape);
     cout << "How many decimal places should the area be? ";
     cin >> decimalPlaces;
-    cout << setprecision(decimalPlaces) << fixed;
+    cout << setprecision(decimalPlaces) << fixed; //Sets the number of decimal values based on the user's entry 
+    
+    //User's shape entry will determine which area function is called to calculate the area
     if(shape == "Triangle" || shape == "triangle") 
     {
         areaOfTraingle();
@@ -85,7 +94,7 @@ int main()
         areaOfTrapezoid();
     }
     else{
-        cout << "Can't compute that area." << endl;
+        cout << "Can't compute that area." << endl; //If the user enters an invalid shape or string
     }
 
     return 0;
